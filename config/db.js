755154da1679d1d4d794ef1557fbb8309b03b184
Manish Chandolu <109282238+manishcodes21@users.noreply.github.com>
2024-06-05 19:06:08 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import colors from 'colors';
 const connectDB=async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URl);
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(`Connected to MongoDB ${conn.connection.host}`.bgMagenta.white)
     }
     catch(error){
